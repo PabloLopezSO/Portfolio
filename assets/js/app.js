a5de00ff93b65b578552c296c59dd5315bd80f88
PabloLopezSO <[luis.lopez@softwareone.com]>
2022-02-03 13:28:10 +0100

    Major Changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,42 +34,6 @@ function toggleHam(){
     appear[0].classList.toggle("active-menu");
 
 }
-//DARK MODE
-
-function changeMode(){
-
-    let ball = document.getElementsByClassName('mode-container__item--ball');
-    let bg = document.getElementsByClassName('mode-container__item');
-    ball[0].classList.toggle("changeMode");
-    bg[0].classList.toggle("changeColor");
-
-}
-
-function darkMode(){
-
-    let theme = document.documentElement.getAttribute('data-theme');
-    let logo = document.getElementsByClassName('logo-container');
-    let target = "dark";
-    if (theme === "dark") {
-        
-        target = "light";
-       logo[0].style.backgroundImage="url(assets/img/logol.png)";
-
-    } 
-
-    document.documentElement.setAttribute('data-theme', target);
-}
-
-// Ham Menu
-
-function toggleHam(){
-
-    let appear = document.getElementsByClassName('main-nav');
-    appear[0].classList.toggle("active-menu");
-
-}
-
-
 
 const swiper = new Swiper('.swiper', {
 
@@ -84,3 +48,9 @@ const swiper = new Swiper('.swiper', {
     }
 
   });
+
+// Button
+
+function buttonSmooth(content){
+    document.getElementById(content).scrollIntoView({behavior: 'smooth'});
+}
